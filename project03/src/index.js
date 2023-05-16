e0dee2fedc75f938b59601af1c3d4198e0a12fc7
orthh <witchicken2@gmail.com>
2023-05-16 12:41:25 +0900

    docs : useState 기본

--- project03/src/index.js
+++ project03/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 01. useState를 사용하는 이유 및 개념(+리액트 이벤트)
+import App from "./Ex01";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
